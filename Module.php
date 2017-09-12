3cbdd4b356a3c5ebf6e38460a9324a3841a35a9e
vasil <vasil.sokolov@gmail.com>
2017-09-12 15:16:00 +0300

    some improvements

--- Module.php
+++ Module.php
@@ -1,46 +1,40 @@
 <?php
 
-namespace Aurora\Modules\LicensingTiral;
-
-//include_once __DIR__.'/classes/KI.php';
+namespace Aurora\Modules\LicensingTrial;
 
 class Module extends \Aurora\System\Module\AbstractModule
 {
 	public function init() 
 	{
-//		$this->oApiAccountsManager = new Managers\Accounts\Manager($this);
-
 		$this->subscribeEvent('Licensing::GetLicenseKey::after', array($this, 'onAfterGetLicenseKey'));
 	}
 	
 	public function onAfterGetLicenseKey($Args, &$Result)
 	{
-		var_dump($Result);
-	}
-	
-	public function GetSettings()
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::SuperAdmin);
-		
-		return array(
-			'LicenseKey' => $this->GetLicenseKey(),
-		);
-	}
-	
-	/**
-	 * @return bool
-	 */
-	public function UpdateSettings($LicenseKey)
-	{
-		$bResult = false;
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::SuperAdmin);
-		
-		if ($LicenseKey !== null)
+		if (empty($Result))
 		{
-			\Aurora\System\Api::GetSettings()->SetConf('LicenseKey', $LicenseKey);
-			$bResult = \Aurora\System\Api::GetSettings()->Save();
+			$sRemoteUrl = $this->getConfig('RemoteHost', '');
+			
+			$oResponse = empty($sRemoteUrl) ? null : json_decode(\file_get_contents($sRemoteUrl));
+			
+			if ($oResponse)
+			{
+				if (isset($oResponse->success) && $oResponse->success === true && !empty($oResponse->key))
+				{
+					$Result = $oResponse->key;
+
+					$oLicensingDecorator = \Aurora\System\Api::GetModuleDecorator('Licensing');
+
+					if ($oLicensingDecorator)
+					{
+						$oLicensingDecorator->UpdateSettings($Result);
+					}
+				}
+				else
+				{
+					\Aurora\System\Api::Log('Bad response from Key Service', \Aurora\System\Enums\LogLevel::Error);
+				}
+			}
 		}
-		
-		return $bResult;
 	}
 }
